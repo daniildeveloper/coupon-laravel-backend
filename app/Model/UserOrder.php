33680d89d57fa9d1079f1e20bd7e45e6bceb2b28
daniiltserin <daniilborowkow@yandex.ru>
2017-07-22 18:17:58 +0600

    Created base orders entity

---
+++ app/Model/UserOrder.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserOrder extends Model
+{
+    protected $table = 'user_orders';
+}
---
+++ database/migrations/2017_07_22_115026_create_user_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->text('cart');
+            $table->intege('status_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_orders');
+    }
+}
