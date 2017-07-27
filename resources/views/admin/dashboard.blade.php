af974b0b987f105c3197f8664a163b6c5b5fa588
daniiltserin <daniilborowkow@yandex.ru>
2017-07-27 23:02:30 +0600

    Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -77,105 +77,13 @@
         </span>
     </div> --}}
 </div>
-<!-- /top tiles -->
-<div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="dashboard_graph">
-            <div class="row x_title">
-                <div class="col-md-6">
-                    <h3>
-                        Покупки
-                        <small>
-                            и посещения (демо данные)
-                            {{-- TODO: real data --}}
-                        </small>
-                    </h3>
-                </div>
-                <div class="col-md-6">
-                    <div class="pull-right" id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-                        <i class="glyphicon glyphicon-calendar fa fa-calendar">
-                        </i>
-                        <span>
-                            December 30, 2014 - January 28, 2015
-                        </span>
-                        <b class="caret">
-                        </b>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-9 col-sm-9 col-xs-12">
-                <div class="demo-placeholder" id="chart_plot_01">
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
-                <div class="x_title">
-                    <h2>
-                        Источники трафика
-                    </h2>
-                    <div class="clearfix">
-                    </div>
-                </div>
-                <div class="col-md-12 col-sm-12 col-xs-6">
-                    <div>
-                        <p>
-                            С Ядекс
-                        </p>
-                        <div class="">
-                            <div class="progress progress_sm" style="width: 76%;">
-                                <div class="progress-bar bg-green" data-transitiongoal="80" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <p>
-                            С Google
-                        </p>
-                        <div class="">
-                            <div class="progress progress_sm" style="width: 76%;">
-                                <div class="progress-bar bg-green" data-transitiongoal="60" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-12 col-sm-12 col-xs-6">
-                    <div>
-                        <p>
-                            Таргет Вконтакте
-                        </p>
-                        <div class="">
-                            <div class="progress progress_sm" style="width: 76%;">
-                                <div class="progress-bar bg-green" data-transitiongoal="40" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <p>
-                            Личные встречи
-                        </p>
-                        <div class="">
-                            <div class="progress progress_sm" style="width: 76%;">
-                                <div class="progress-bar bg-green" data-transitiongoal="50" role="progressbar">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="clearfix">
-            </div>
-        </div>
-    </div>
-</div>
 <br/>
 <div class="row">
         <div class="col-md-4 col-sm-4 col-xs-12">
         <div class="x_panel tile fixed_height_320 overflow_hidden">
             <div class="x_title">
                 <h2>
-                    Посещения
+                    Посещения (See ideas)
                 </h2>
                 <div class="clearfix">
                 </div>
